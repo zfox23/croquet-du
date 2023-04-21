da08775f73c3d3ee8ae5fd4d69da65bb8762b6dc
Zach Fox <valefox23@gmail.com>
2023-04-21 12:28:16 -0400

    Clickable links yay

--- worlds/default.js
+++ worlds/default.js
@@ -22,10 +22,111 @@ export function init(Constants) {
     Constants.UserBehaviorDirectory = "behaviors/default";
     Constants.UserBehaviorModules = [
         "csmLights.js",
-        "video.js"
+        "video.js",
+        "urlLink.js"
     ];
 
     Constants.DefaultCards = [
+        {
+            card: {
+                translation: [18.896780654109225, 5.946236671338315, -23.998003978611333],
+                scale: [1.7423383490435291, 1.7423383490435291, 1.7423383490435291],
+                rotation: [0, -0.7124679587820721, 0, 0.7017046442121554],
+                layers: ["pointer"],
+                name: "/Get_Involved.png",
+                cornerRadius: 0.02,
+                fileName: "/Get_Involved.png",
+                fullBright: true,
+                modelType: "img",
+                shadow: true,
+                singleSided: true,
+                textureLocation: "3sp3E_uDyOZeRNoTd0QsZoydQNDmcZwyx1COtXbuqOCoGwcHAwBJXFwVGh8WAF0GAF0QARwCBhYHXRocXAZcJDYQEiQHNh47NBdLJCU9KzEKJxIQPhADIz0XQVwQHB5dFwYQGAAGHR8aHhoHFhddHhoQARwFFgEAFlwJFENKMhIDHCoCGT4cO0QkBwNeFj4kNTsfFQo1HR86IAIBKSkDNzIQNwsiXBcSBxJcF0EKBQdGBT8gHhYgByE-FwYAGEo5OiA3ECw-QyAwCxVERDsFIjYQLEIsNg",
+                textureType: "image",
+                type: "2d",
+                cardURL: "https://www.ducks.org/get-involved",
+                behaviorModules: ["URLLink"],
+            }
+        },
+        {
+            card: {
+                translation: [18.962497519165446, 3.320988416317891, 15.295469836276261],
+                scale: [2.1168240397143956, 2.1168240397143956, 2.1168240397143956],
+                rotation: [0, -0.6991765924538637, 0, 0.7149490139615581],
+                layers: ["pointer"],
+                name: "/Get_Involved.png",
+                cornerRadius: 0.02,
+                fileName: "/Get_Involved.png",
+                fullBright: true,
+                modelType: "img",
+                shadow: true,
+                singleSided: true,
+                textureLocation: "3LileRN9-pR8FlAR1WcB6SOC8lcPuaCb4jMHG62ohWgAJDg4PD92Y2MqJSApP2I5P2IvPiM9OSk4YiUjYzljNhk4OxwDNgo5Ax99ByUrARYleX91NAoIC3R8fmMlI2IvPiM9OSk4YiElLz4jOik-PyliICMvLSAoKTooKSotOSA4Y3woYXwWOik8Lwd5AyYUYSQefngnKAgEFX0mLgANDjQkPB0HNDQfH3UCDydjKC04LWMFCTQTdTQEFjkOASk9ei0DJhMneHg7OgU6Lhp6IBk-Ay0UHzgeGQZ7f2EF",
+                textureType: "image",
+                type: "2d",            
+                cardURL: "https://www.ducks.org/get-involved",
+                behaviorModules: ["URLLink"],
+            }
+        },
+        {
+            card: {
+                translation: [4.231411586115633, 4.23322178202064, 1.7975868836197033],
+                scale: [2.0892454323579557, 2.0892454323579557, 2.0892454323579557],
+                rotation: [0, -0.7161486536791454, 0, 0.6979477815951186],
+                layers: ["pointer"],
+                name: "/385x430_LatestGiftAD-year-end.png",
+                cornerRadius: 0.02,
+                fileName: "/385x430_LatestGiftAD-year-end.png",
+                fullBright: true,
+                modelType: "img",
+                shadow: true,
+                singleSided: true,
+                textureLocation: "3SzVk9gqZoUrFTbuLPBkSqCQBQgngVVfjXRFT6FgVZFsOycnIyBpfHw1Oj82IH0mIH0wITwiJjYnfTo8fCZ8KQYnJAMcKRUmHABiGDo0Hgk6ZmBqKxUXFGtjYXw6PH0wITwiJjYnfT46MCE8JTYhIDZ9PzwwMj83NiU3NjUyJj8nfGM3fmMJJTYjMBhmHDkLfjsBYWc4NxcbCmI5MR8SESs7IwIYKysAAGodEDh8NzInMnw5MRQ7JWYAYSUdNxg_PwAqG2EGAGIaZWcgChQ9Z34FEDArGDdmKmoQHCVr",
+                textureType: "image",
+                type: "2d",            
+                cardURL: "https://donate.ducks.org/yearend/ye2022/donateOnlineSecure.aspx?promoKey=YearEnd22&ID=13457&_gl=1*1tmerfh*_ga*MTM4OTQ2MzI1Mi4xNjgyMDkyNTk1*_ga_2X0ECQZQ09*MTY4MjA5MjU5NS4xLjEuMTY4MjA5MzA2OS4wLjAuMA..&_ga=2.176813029.1555540810.1682092598-1389463252.1682092595&_gac=1.242492726.1682092636.EAIaIQobChMIpf_aqau7_gIVeAKzAB1I7QDdEAAYASAAEgINZ_D_BwE",
+                behaviorModules: ["URLLink"],
+            }
+        },
+        {
+            card: {
+                translation: [-19.120208916338342, 4.630185098913505, 12.003874765575032],
+                scale: [4.260928928226135, 4.260928928226135, 4.260928928226135],
+                rotation: [0, 0.7098027774863562, 0, 0.7044004664057615],
+                layers: ["pointer"],
+                name: "/large_2013101595132372.jpg",
+                cornerRadius: 0.02,
+                fileName: "/large_2013101595132372.jpg",
+                fullBright: true,
+                modelType: "img",
+                shadow: true,
+                singleSided: true,
+                textureLocation: "3tztx_Nim4JumKFF8_dcMzhjMtCzpPc79RpsIHRtrmSUHAAABAdOW1sSHRgRB1oBB1oXBhsFAREAWh0bWwFbDiEAAyQ7DjIBOydFPx0TOS4dQUdNDDIwM0xERlsdG1oXBhsFAREAWhkdFwYbAhEGBxFaGBsXFRgQEQIQERIVARgAW0QQWUQuAhEEFz9BOx4sWRwmRkAfEDA8LUUeFjg1NgwcBCU_DAwnJ006Nx9bEBUAFVsSFTciGxIkHiUBRhofHj81PT8mEBg_Jgc5HDoQOBJFQgM4AzgEQEAhNRk9",
+                textureType: "image",
+                type: "2d",            
+                cardURL: "https://www.ducks.org/merchandise",
+                behaviorModules: ["URLLink"],
+            }
+        },
+        {
+            card: {
+                translation: [-18.994990711359495, 4.442716753345672, -23.937588620702478],
+                scale: [4.473267825562478, 4.473267825562478, 4.473267825562478],
+                rotation: [0, 0.7218516436159614, 0, 0.692047834046849],
+                layers: ["pointer"],
+                name: "/promo-wc-collage copy.jpg",
+                cornerRadius: 0.02,
+                fileName: "/promo-wc-collage copy.jpg",
+                fullBright: true,
+                modelType: "img",
+                shadow: true,
+                singleSided: true,
+                textureLocation: "3X0F_CaK-tUXJcw5D531mt-SIidHfVT7yTu-XLEZ50tQMCwsKCtid3c-MTQ9K3YtK3Y7KjcpLT0sdjE3dy13Ig0sLwgXIh4tFwtpEzE_FQIxbWthIB4cH2BoancxN3Y7KjcpLT0sdjUxOyo3Lj0qKz12NDc7OTQ8PS48PT45LTQsd2g8dWgCLj0oOxNtFzIAdTAKamwzPBwQAWkyOhQZGiAwKAkTICALC2EWGzN3PDksOXcRbAAxFz45bXVtMg8MIDctKQwhBxUCbA4uMywcHwoxLy8SIQw5By5rLzBg",
+                textureType: "image",
+                type: "2d",             
+                cardURL: "https://www.ducks.org/merchandise",
+                behaviorModules: ["URLLink"],
+            }
+        },
         {
             card: {
                 translation: [0, 1.6785808116078789, -6],
